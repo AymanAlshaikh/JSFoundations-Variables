d6790406abee2ce8cbfa450a2515b57edd55ce07
AymanAlshaikh <ayman159@live.com>
2021-01-10 14:15:07 +0300

    Done

--- Story.js
+++ Story.js
@@ -1,4 +1,4 @@
 let name = 'Ayman';
 let age = 23;
 let hobby = 'Coding';
-console.log(`Mr. ${name}, is ${age} years old, you will be ${age +33} by then you will have decades of ${hobby} practice!`)
+console.log(`Mr. ${name}, is ${age}, in 33 years you will be ${age+33}, by then you will have decades of ${hobby} practice`)
