deb56af9fcc30ab58f3d63946684567d78d6b595
AymanAlshaikh <ayman159@live.com>
2021-01-10 13:18:34 +0300

    Variables done

---
+++ Story.js
@@ -0,0 +1,4 @@
+let name = 'Ayman';
+let age = 23;
+let hobby = 'Coding';
+console.log(`Mr. ${name}, is ${age} years old, you will be ${age +33} by then you will have decades of ${hobby} practice!`)
